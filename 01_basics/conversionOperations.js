6875212c3abf6e5d1ecebb452f898626bf4ad3e2
Arshad Hussain Gilal <129890701+Arshad-Hussain27@users.noreply.github.com>
2024-02-28 10:14:01 +0000

    Conversion of Datatype

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,21 @@
+// let score = 43;
+// console.log(typeof score);
+// console.log(typeof (score));
+// 33 => 33;
+// boolean => 1 true 0 false;
+// "33AJJASHF" => NaN;
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// let isLoggedIn = "arshad";
+// let BooleanIsLoggedIn = Boolean(isLoggedIn);
+// console.log(BooleanIsLoggedIn);
+
+let someNumber = 55;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
