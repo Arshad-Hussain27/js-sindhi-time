f5ed1c9522802ab500bef68fd187208e11b988f4
Arshad Hussain Gilal <129890701+Arshad-Hussain27@users.noreply.github.com>
2024-02-28 03:16:58 +0000

    datatype

---
+++ 01_basics/datatype.js
@@ -0,0 +1,18 @@
+"use strict"; // treat all js code in newer verion
+
+//alert(3 + 3);
+
+// All Data types
+
+//number => 2 power of 54 
+// bigint 
+// string => ""
+// boolean => true/false
+// null standalone value
+// undefined => \
+//  symbol => unique 
+
+
+// objactive 
+
+console.log(typeof "Arshad");
---
+++ 01_basics/veriable.js
@@ -0,0 +1,12 @@
+const accountID = 12345;
+let accountName = "Arshad";
+var accountPass = "aarrss";
+accountCity = "hyerabad";
+
+
+accountName = "Baseer";
+accountPass = "8665967";
+accountCity = "karachi";
+
+
+console.table([accountID , accountName , accountPass]);
