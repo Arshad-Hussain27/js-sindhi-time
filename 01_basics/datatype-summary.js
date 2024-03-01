34d2319ea77811e30ba92019d9611123b7be245a
Arshad Hussain Gilal <129890701+Arshad-Hussain27@users.noreply.github.com>
2024-03-01 10:53:38 +0000

    Basic Of Data Type

---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,27 @@
+// 7 Primitive Datatypes
+
+// 1: String 2: boolean 3: Number 4: null 5: Undefined 6: BigInt 7: Symbol;
+
+const id = Symbol('123');
+const anotherId = Symbol('1235');
+
+//console.log(id === anotherId);
+
+// Reference (Non-Primitive);
+
+// Objects Functions Array;
+
+//const friends = ["Arshad", "Baseer", "Uzair"];
+ 
+//let myOnj = {
+    //Name: "Najam",
+    //Age: 25,
+  //  Class:  BSIT,
+
+
+const myFunction = function(){
+    console.log("Hello Word");
+
+}
+
+console.log(typeof myFunction);
